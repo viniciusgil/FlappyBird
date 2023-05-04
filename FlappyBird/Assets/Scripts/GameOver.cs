b850a5300e5d24fd846fce081b40934dfd9c910e
viniciusgil <viniciusgil_es@hotmail.com>
2023-05-04 14:19:55 +0100

    .

--- FlappyBird/Assets/Scripts/GameOver.cs
+++ FlappyBird/Assets/Scripts/GameOver.cs
@@ -6,32 +6,20 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 {
     public int gameStartScene;
-
-
     public void StartGame()
     {
         gameObject.SetActive(false);
         SceneManager.LoadScene(gameStartScene);
 
     }
-   /* public void Restart()
-    {
-        Debug.Log("Restart");
-        gameObject.SetActive(false);
-
-        GameManager.Instance.Enable();
-    }
-   */
-
-    public void Quit()
+     public void Quit()
     {
         // This will test if the quit button works. 
        // UnityEditor.EditorApplication.isPlaying= false;
         Application.Quit();
         Debug.Log("Quit!");
     }
-
-    
+        
     public AudioSource audio;
 
     public void playButton()
